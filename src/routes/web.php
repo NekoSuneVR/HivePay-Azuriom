dfe67f55792954168b27dd38ce3d0a5cb52ec44f
NekoSuneVR <Chisdeal2014@hotmail.com>
2025-10-04 15:46:44 +0100

    Create web.php

---
+++ src/routes/web.php
@@ -0,0 +1,5 @@
+use Illuminate\Support\Facades\Route;
+use Azuriom\Plugin\HivePay\HiveMethod;
+
+Route::post('/hivepay/notify/{payment?}', [HiveMethod::class, 'notification'])
+    ->name('hivepay.notify');
