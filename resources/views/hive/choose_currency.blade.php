dbeb433a28492f00e65f2bd3a9eab495929cd3c4
NekoSuneVR <Chisdeal2014@hotmail.com>
2025-10-04 14:54:28 +0100

    Create choose_currency.blade.php

---
+++ resources/views/hive/choose_currency.blade.php
@@ -0,0 +1,22 @@
+@extends('shop::layouts.app')
+
+@section('title', 'Choose Payment Currency')
+
+@section('content')
+<div class="container text-center my-5">
+    <h2>Select Currency</h2>
+    <p>You will pay <strong>{{ $amount }}</strong> in your chosen currency.</p>
+
+    <form method="POST" action="{{ $form_action }}">
+        @csrf
+        <input type="hidden" name="amount" value="{{ $amount }}">
+        <div class="my-3">
+            @foreach($currencies as $cur)
+                <button type="submit" name="pay_currency" value="{{ $cur }}" class="btn btn-outline-primary m-2">
+                    Pay with {{ $cur }}
+                </button>
+            @endforeach
+        </div>
+    </form>
+</div>
+@endsection
