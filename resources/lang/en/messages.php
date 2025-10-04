14e7df9d8f345b09a9822e08f5bc30cbd0fb89bb
NekoSuneVR <Chisdeal2014@hotmail.com>
2025-10-04 13:00:34 +0100

    Create messages.php

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,8 @@
+return [
+    'account' => 'Hive Account',
+    'currency' => 'Currency',
+    'rpc' => 'Hive RPC Endpoint',
+    'setup' => 'Set your Hive wallet to receive payments. Use the notification URL :notification for confirmations.',
+    'memo' => 'When a customer pays, they must include the payment ID in the memo.',
+    'memo_format' => 'Payment ID: :id',
+];
